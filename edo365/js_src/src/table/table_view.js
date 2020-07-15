6b0e1f23b74ba275a4e34ca6141c52b5c9dfe5e5
Benjamin Benno Falkner <benno.falkner@gmail.com>
2020-07-15 19:20:32 +0200

    fix

--- edo365/js_src/src/table/table_view.js
+++ edo365/js_src/src/table/table_view.js
@@ -68,7 +68,7 @@ export class TableView {
     get_selected(){
         var r = [];
         for(var i = 1; i < this.target.childElementCount; i++){
-            if(this.target.childNodes[i].childNodes[0].checked)
+            if(this.target.childNodes[1].childNodes[0].childNodes[0].checked)
                 r.push(i-1);
         }
         return r;
